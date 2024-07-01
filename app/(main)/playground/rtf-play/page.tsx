0379f5c862f8e6145bf40b9b7859cd63c9304b25
Luoie8340 <itslukiee@gmail.com>
2024-07-01 18:12:28 +0800

    rtf-play page initial

--- app/(main)/playground/rtf-play/page.tsx
+++ app/(main)/playground/rtf-play/page.tsx
@@ -1,7 +1,37 @@
-export default function page() {
+"use client";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Mesh } from "three";
+
+export default function layout() {
   return (
-    <section className="flex h-[100vh] items-center justify-center overflow-hidden">
-      <p>rtf-play</p>
-    </section>
+    <div className="h-[100vh]">
+      <Canvas>
+        <OrbitControls enableZoom={false} enablePan={false} />
+        <ambientLight intensity={1} />
+        <directionalLight position={[3, 2, 1]} />
+        <Cube />
+      </Canvas>
+    </div>
+  );
+}
+
+function Cube() {
+  const mesh = useRef<Mesh>(null);
+
+  useFrame((state, delta) => {
+    if (mesh.current) {
+      mesh.current.rotation.x += delta * 0.25;
+      mesh.current.rotation.y += delta * 0.25;
+      mesh.current.rotation.z += delta * 0.25;
+    }
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry args={[2.5, 2.5, 2.5]} />
+      <meshStandardMaterial color={"white"} />
+    </mesh>
   );
 }
